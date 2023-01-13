5fc37c189aa7df5e67f0f27687b4e52d0a6b5df6
kimminji <kimminji5634@gmail.com>
2023-01-13 23:39:45 +0900

    톰캣 사용을 위한 application 추가

--- src/main/java/com/project/gongyeon2jo/Gongyeon2joApplication.java
+++ src/main/java/com/project/gongyeon2jo/Gongyeon2joApplication.java
@@ -2,12 +2,17 @@ package com.project.gongyeon2jo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Gongyeon2joApplication {
+public class Gongyeon2joApplication extends SpringBootServletInitializer { // 상속
+        public static void main(String[] args) {
+            SpringApplication.run(Gongyeon2joApplication.class, args);
+        }
+        @Override // 상속, 오버라이드 => 톰캣 실행을 위해서
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+            return super.configure(builder);
+        }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Gongyeon2joApplication.class, args);
     }
-
-}
