f66eda52bda49346fe92e7425874200d8e5f019a
chenj <chenj7498@gmail.com>
2023-01-30 20:00:52 +0900

    예매하기 좌석선택 추가

--- src/main/java/com/project/gongyeon2jo/ticketing/controller/TicketingController.java
+++ src/main/java/com/project/gongyeon2jo/ticketing/controller/TicketingController.java
@@ -5,12 +5,14 @@ import com.project.gongyeon2jo.performance.service.PerformanceService;
 import com.project.gongyeon2jo.ticketing.model.Ticketing;
 import com.project.gongyeon2jo.ticketing.service.TicketingService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/ticketing")
@@ -21,18 +23,26 @@ public class TicketingController {
     private final PerformanceService performanceService;
 
     @GetMapping("")
-    public ModelAndView getTicketing(Integer performanceId) {
+    public ModelAndView seat(Integer performanceId) {
         ModelAndView modelAndView = new ModelAndView();
 //        Optional<Performance> performance = performanceService.getPerformance(performanceId);
 //        modelAndView.addObject("performance", performance.get());
         Performance detail = performanceService.detail(performanceId);
         modelAndView.addObject("detail", detail);
-        modelAndView.setViewName("ticketing/detail");
+        modelAndView.setViewName("ticketing/seat");
         return modelAndView;
     }
 
-    @PostMapping("")
-    public ModelAndView insert(Ticketing ticketing) {
+    @PostMapping("/order")
+    public ModelAndView order(Ticketing ticketing) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("ticketing", ticketing);
+        modelAndView.setViewName("ticketing/order");
+        return modelAndView;
+    }
+
+    @PostMapping("/complete")
+    public ModelAndView complete(Ticketing ticketing) {
         ModelAndView modelAndView = new ModelAndView();
         ticketingService.insert(ticketing);
         modelAndView.addObject("ticketing", ticketing);
